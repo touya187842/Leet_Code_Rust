4aa2d2462b0eb6f4ce917a5396112184d2e5aff1
touya187842 <touya187842@yahoo.co.jp>
2022-11-27 15:27:24 +0800

    feat add tree node
    as binary complete tree

--- src/leet_code.rs
+++ src/leet_code.rs
@@ -1,3 +1,6 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
   pub val: i32,
@@ -30,5 +33,61 @@ impl ListNode {
       next = &node.next;
     }
     vec
+  }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+
+  pub fn from_slice(vals: &[Option<i32>]) -> Option<Rc<RefCell<Self>>> {
+    Self::from_slice_inner(vals, 0)
+  }
+
+  fn from_slice_inner(vals: &[Option<i32>], index: usize) -> Option<Rc<RefCell<Self>>> {
+    if index < vals.len() && vals[index].is_some() {
+      return Some(Rc::new(RefCell::new(TreeNode {
+        val: vals[index].unwrap(), 
+        left: Self::from_slice_inner(vals, (index << 1) + 1),
+        right: Self::from_slice_inner(vals, (index << 1) + 2) 
+      })))
+    }
+    None
+  }
+
+  pub fn to_vec(&self) -> Vec<Option<i32>> {
+    let mut vec: Vec<Option<i32>> = Vec::new();
+    vec.push(Some(self.val));
+    Self::push_to_vec(&self.left, &mut vec, 1);
+    Self::push_to_vec(&self.right, &mut vec, 2);
+    vec
+  }
+
+  fn push_to_vec(node: &Option<Rc<RefCell<Self>>>, vec: &mut Vec<Option<i32>>, index: usize) {
+    if node.is_none() {
+      return;
+    }
+
+    while vec.len() <= index {
+        vec.push(None);
+    }
+
+    let inner = node.as_ref().unwrap().borrow();
+    vec[index] = Some(inner.val);
+    Self::push_to_vec(&inner.left, vec, (index << 1) + 1);
+    Self::push_to_vec(&inner.right, vec, (index << 1) + 2);
   }
 }
--- tests/leet_code_test.rs
+++ tests/leet_code_test.rs
@@ -1,4 +1,5 @@
 use leet_code_rust::leet_code::ListNode;
+use leet_code_rust::leet_code::TreeNode;
 
 #[test]
 pub fn build_list_node_from_slice_and_to_vec() {
@@ -6,4 +7,14 @@ pub fn build_list_node_from_slice_and_to_vec() {
     assert_eq!(ListNode::from_slice(slice1).map_or(Vec::new(),|x| x.to_vec()), slice1);
     let slice2: &[i32] = &[];
     assert_eq!(ListNode::from_slice(slice2).map_or(Vec::new(),|x| x.to_vec()), slice2);
+}
+
+#[test]
+pub fn build_tree_from_slice_and_to_vec() {
+    let slice1: &[Option<i32>] = &[Some(1), None, Some(6), None, None, Some(3)];
+    assert_eq!(TreeNode::from_slice(slice1).map_or(Vec::new(),|x| x.borrow().to_vec()), slice1);
+    let slice2: &[Option<i32>] = &[];
+    assert_eq!(TreeNode::from_slice(slice2).map_or(Vec::new(),|x| x.borrow().to_vec()), slice2);
+    let slice3: &[Option<i32>] = &[Some(3), None, None, Some(2)];
+    assert_eq!(TreeNode::from_slice(slice3).map_or(Vec::new(),|x| x.borrow().to_vec()), slice3[..1]);
 }
